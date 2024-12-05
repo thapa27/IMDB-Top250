996cd0c310847bd42c89cd5f0fc2c11c810c0650
aayush <aayushthapa70@gmail.com>
2024-12-05 13:32:34 -0500

    Added requests headers while fetching data from each team URL

--- epl-eachTeam.py
+++ epl-eachTeam.py
@@ -1,7 +1,8 @@
 import requests, csv
 from bs4 import BeautifulSoup
+import time
 
-def get_each_team_stats(url, session):
+def get_table_headers(url, session):
     response = session.get(url)
     if response.status_code != 200:
         print('Failed to fetch the webpage. Status Code: ', response.status_code)
@@ -11,15 +12,37 @@ def get_each_team_stats(url, session):
 
     headers = [th.text.strip() for th in table.find('thead').find_all('th') if th]
 
+    return headers
+
+def get_each_team_stats(url, session):
+    response = session.get(url)
+    print('Fetching data from ', url)
+
+    if response.status_code != 200:
+        print('Failed to fetch the webpage. Status Code: ', response.status_code)
+        exit()
+    soup = BeautifulSoup(response.content, 'lxml')
+    table = soup.find('table', id="matchlogs_for")
+
     body = []
 
     for row in table.find('tbody').find_all('tr'):
         body.append([td.text for td in row.find_all(['th', 'td'])])
 
-    return headers, body
+    return body
 
+
+requests_headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Referer": "https://fbref.com/en/comps/9/Premier-League-Stats",
+    "Upgrade-Insecure-Requests": "1",
+}
 
 session = requests.Session()
+session.headers.update(requests_headers)
 
 # Fetching a premier league overall stats table
 url="https://fbref.com/en/comps/9/Premier-League-Stats"
@@ -28,6 +51,7 @@ response = session.get(url)
 # Exiting if connection failed
 if response.status_code != 200:
     print("Failed to fetch the page! Status code: ", response.status_code)
+    print(response.headers)
     exit()
 
 
@@ -41,16 +65,22 @@ team_stats_url = [row.find('a').get('href') for row in table.find_all('td', attr
 team_names = [row.find('a').text for row in table.find_all('td', attrs={'data-stat':'team'})]
 team_stats_url = [ "https://fbref.com" + team  for team in team_stats_url]
 
-print(team_names)
+headers = get_table_headers(team_stats_url[0], session)
 
-# headers, _ = get_each_team_stats(team_stats_url[0], session)
 
-# with open('team-stats.csv', mode='a', newline="", encoding='utf-8') as file:
-#     writer = csv.writer(file)
-#     writer.writerow(headers)
-#     for team in team_stats_url[:1]:
-#         _, body = get_each_team_stats(team, session)
-#     writer.writerows(body)
+with open('team-stats.csv', mode='w', newline="", encoding='utf-8') as file:
+    writer = csv.writer(file)
+    i = 0
+    for team in team_stats_url:
+        writer.writerow([team_names[i]])
+        body = get_each_team_stats(team, session)
+        writer.writerow(headers)
+        writer.writerows(body)
+        writer.writerow([])
+        writer.writerow([])
+        i = i+1
+        time.sleep(10)
+    
 
 session.close()
 
